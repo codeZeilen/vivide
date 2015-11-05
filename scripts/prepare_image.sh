fe0e8bce7adaa3b01653c5f3288e0d5c4050cf36
Marcel Taeumel <marcel.taeumel@hpi.uni-potsdam.de>
2015-11-05 13:07:55 +0100

    [ci skip] Names for build artifacts updated

--- scripts/prepare_image.sh
+++ scripts/prepare_image.sh
@@ -28,8 +28,8 @@ fi
 # Set paths and files
 # ==============================================================================
 DEPLOY_PATH="$SMALLTALK_CI_HOME/deploy"
-VIVIDE_IMAGE="Vivide-$SMALLTALK.image"
-VIVIDE_CHANGES="Vivide-$SMALLTALK.changes"
+VIVIDE_IMAGE="Vivide-4.0-$SMALLTALK.image"
+VIVIDE_CHANGES="Vivide-4.0-$SMALLTALK.changes"
 DEPLOY_TARGET="https://www.hpi.uni-potsdam.de/hirschfeld/artefacts/vivide/"
 COG_VM_PARAM=""
 case "$(uname -s)" in
